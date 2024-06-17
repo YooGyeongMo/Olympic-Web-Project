9510b0061c953e9841f8e009f7fe4e653108e5d4
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2024-06-17 15:04:28 +0900

    수정

--- Safety-Paris-React/safetyparis/src/components/Map.js
+++ Safety-Paris-React/safetyparis/src/components/Map.js
@@ -3,29 +3,31 @@ import "./Map.css";
 
 const Map = () => {
   useEffect(() => {
-    // 스크립트 로드 함수
-    const loadScript = (url) => {
-      let script = document.createElement("script");
-      script.type = "text/javascript";
-      script.async = true; // 비동기 로딩 설정
-      script.onload = console.log("Google Maps script loaded.");
-      script.src = url;
-      document.head.appendChild(script);
-    };
-
-    // initMap 함수
-    const initMap = () => {
+    // 전역 네임스페이스에 initMap을 할당
+    window.initMap = () => {
       new window.google.maps.Map(document.getElementById("map"), {
         center: { lat: 48.8575, lng: 2.3514 },
         zoom: 13,
       });
     };
 
+    // 스크립트 로드 함수
+    const loadScript = (url) => {
+      const scriptExists = document.querySelector(`script[src="${url}"]`);
+      if (!scriptExists) {
+        let script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = url;
+        script.async = true;
+        script.onload = () => console.log("Google Maps API 로드 완료");
+        document.head.appendChild(script);
+      }
+    };
+
     // Google Maps 스크립트 로드
     loadScript(
       `https://maps.googleapis.com/maps/api/js?key=AIzaSyCxCjOgsPDF__iNago8obFLPRIgotaAjsA&callback=initMap`
     );
-    window.initMap = initMap; // 전역 네임스페이스에 initMap을 할당합니다.
   }, []);
 
   return <div id="map"></div>;
